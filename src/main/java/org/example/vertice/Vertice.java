fd5ce7cb9a9381886634f8de446c482750cf4677
TheoMinariniDeMelo <theominarini@outlook.com>
2023-08-04 15:43:19 -0300

    Create Fork of GeometricPitagoric

--- src/main/java/org/example/vertice/Vertice.java
+++ src/main/java/org/example/vertice/Vertice.java
@@ -1,34 +1,42 @@
 package org.example.vertice;
 
+import org.example.services.GeometricPitagoric;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Vertice {
-    public void selectVertice(double[] citieCord, List<String> verticeCoordinates) {
+public class Vertice extends GeometricPitagoric {
+    public Double selectVertice(double[] citieCord, List<String> verticeCoordinates) {
         List<Double[]> coordinates = parseCoordinates(verticeCoordinates);
+        return fork(coordinates, citieCord);
     }
 
-    protected List<Double[]> grafo(List<Double[]> coordinates, double[] citieCord) {
+    protected Double fork(List<Double[]> coordinates, double[] citieCord) {
         List<Double[]> cordieParam = new ArrayList<>();
         coordinates.forEach(coordinate -> {
             double result_x = coordinate[0] - citieCord[0];
             double result_y = coordinate[1] - citieCord[1];
             cordieParam.add(new Double[]{result_x, result_y});
         });
-        return cordieParam.stream().filter(x->x[0] != 0 && x[1]!=0).collect(Collectors.toList());
+        this.setHipotenuse(cordieParam.stream()
+                .sorted(Comparator.comparing(x -> x[0]))
+                .toList());
+        return this.hipotenuse.stream().sorted().toList().get(0);
     }
 
     protected static List<Double[]> parseCoordinates(List<String> coordinate) {
         List<Double[]> coordinates = new ArrayList<>();
-        
+
         coordinate.forEach(x -> {
             String[] cord = x.split(",");
             Double[] doubleCord = new Double[cord.length];
 
             for (int i = 0; i < cord.length; i++) {
                 doubleCord[i] = Double.parseDouble(cord[i]);
-            };
+            }
+            ;
 
             coordinates.add(doubleCord);
         });
