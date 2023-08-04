611735f79212017b03f4d1a630fcf76dc5b59a31
TheoMinariniDeMelo <theominarini@outlook.com>
2023-08-04 13:39:06 -0300

    Criação do Cli estagio 2

--- src/main/java/org/example/Cli.java
+++ src/main/java/org/example/Cli.java
@@ -1,22 +1,28 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import org.example.enums.Status;
+
+import java.util.*;
 
 public class Cli {
     protected Scanner sc = new Scanner(System.in);
-    protected List<String> cidades = new ArrayList<>();
-    public Cli(){
+    protected Map<String, Integer> cidades = new HashMap<>();
+
+    public Cli() {
         System.out.println("Numero de cidades:");
         int NumeroDeCidades = sc.nextInt();
         setSc(NumeroDeCidades);
     }
 
     public void setSc(int numeroDeCidades) {
-        for (int inx = 0 ; inx < numeroDeCidades; inx++) {
-            System.out.printf("adicionar cidade %d%n", ++inx);
-            cidades.add(sc.next());
+        for (int inx = 0; inx < numeroDeCidades; inx++) {
+            System.out.printf("adicionar cidade e cidades para grafo %d%n", ++inx);
+            try {
+                cidades.put(sc.next(), sc.nextInt());
+                System.out.printf("%n%s%n", Status.PROCESSANDO);
+            } catch (Error error) {
+                System.out.printf("%n%s%n", Status.ERROR);
+            }
         }
     }
 }
