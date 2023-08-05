61cda0ae8cc1f93aa4d2041ea31a9631b80453b0
TheoMinariniDeMelo <theominarini@outlook.com>
2023-08-05 18:41:56 -0300

    Refactory

--- src/main/java/org/example/vertex/Vertex.java
+++ src/main/java/org/example/vertex/Vertex.java
@@ -2,6 +2,8 @@ package org.example.vertex;
 
 import org.example.services.MathGeometric;
 import org.example.services.Sort;
+import org.example.services.ValidateCoordinates;
+import org.example.services.ValidateVerticesOfCity;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -9,31 +11,19 @@ import java.util.List;
 
 public class Vertex {
     public Double selectVertex(Double[] cityCord, List<String> vertexCoordinates) {
-        List<Double[]> coordinates = parseCoordinates(vertexCoordinates);
+        List<Double[]> coordinates = ValidateVerticesOfCity.parseCoordinates(vertexCoordinates);
 
         return fork(coordinates, cityCord);
     }
 
-    private Double fork(@NotNull List<Double[]> coordinates, Double[] cityCord) {
+    private Double fork(List<Double[]> coordinates, Double[] cityCord) {
         List<Double> cordieParam = new ArrayList<>();
 
         coordinates.forEach(coordinate -> cordieParam.add(MathGeometric.euclidean(coordinate, cityCord)));
         return Sort.sortedList(cordieParam);
     }
 
-    private static @NotNull List<Double[]> parseCoordinates(@NotNull List<String> coordinate) {
-        List<Double[]> coordinates = new ArrayList<>();
-        coordinate.forEach(x -> {
-            String[] cord = x.split(",");
-            Double[] doubleCord = new Double[cord.length];
-            for (int i = 0; i < cord.length; i++) {
-                doubleCord[i] = Double.parseDouble(cord[i]);
-            }
-            coordinates.add(doubleCord);
-        });
 
-        return coordinates;
-    }
 }
 
 
